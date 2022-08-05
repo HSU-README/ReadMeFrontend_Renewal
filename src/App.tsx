14d8010ddfff9d5bd93226cd38d88f008a856fe8
2jaebbang <2jaebbang@naver.com>
2022-08-05 23:50:12 +0900

    remove: 안쓰는 파일 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import loadable from '@loadable/component';
 import { RecoilRoot } from 'recoil';
-import Home from 'pages/Home';
 
+const Home = loadable(() => import('pages/Home'));
 function App() {
   return (
     <div className="App">
