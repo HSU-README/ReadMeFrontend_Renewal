36c92fac610c502ef8221b94eacbad4e48df63d6
LINKER <temp2temp@daum.net>
2022-08-06 01:35:56 +0900

    footer 디자인 변경

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,7 +4,7 @@ import Container from './styles';
 export default function Footer() {
   return (
     <Container>
-      <div className="copyright">copyright Team. Pro:Pofol</div>
+      <div className="copyright">copyright © Pro’s Pofol</div>
     </Container>
   );
 }
