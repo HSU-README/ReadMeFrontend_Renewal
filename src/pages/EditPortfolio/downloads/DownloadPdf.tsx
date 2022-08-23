358646ead5308bf8a38d06efa92edc9ef9dde6dd
2jaebbang <72256238+2jaebbang@users.noreply.github.com>
2022-08-24 01:03:39 +0900

    feat: editPortfolio 다운로드 관련 컴포넌트 추가

---
+++ src/pages/EditPortfolio/downloads/DownloadPdf.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import EditPortfolio from './index.js';
+
+function ExportPdf() {
+  return <EditPortfolio />;
+}
+
+export default ExportPdf;
---
+++ src/pages/EditPortfolio/downloads/DownloadPortfolio.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ExportPdf from './DownloadPdf.js';
+
+function DownloadPortfolio() {
+  return (
+    <div>
+      <ExportPdf />
+    </div>
+  );
+}
+
+export default DownloadPortfolio;
