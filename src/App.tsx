0f7610f30611e651e774abfd37da0818c62d895f
2jaebbang <72256238+2jaebbang@users.noreply.github.com>
2022-08-24 18:03:42 +0900

    feat: 포트폴리오 제작, 미리보기 페이지 연동

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import loadable from '@loadable/component';
 import { RecoilRoot } from 'recoil';
+import { Preview } from '@mui/icons-material';
 
 const Home = loadable(() => import('pages/Home'));
 const MyPage = loadable(() => import('pages/MyPage'));
 const SelectPortfolio = loadable(() => import('pages/SelectPage'));
 const AllPortfolio = loadable(() => import('pages/AllPortfolio'));
+const DownloadPortfolio = loadable(() => import('pages/EditPortfolio/downloads/DownloadPortfolio'));
 function App() {
   return (
     <div className="App">
@@ -17,6 +19,10 @@ function App() {
             <Route path="/myPage" element={<MyPage />} />
             <Route path="/select" element={<SelectPortfolio />} />
             <Route path="/all" element={<AllPortfolio />} />
+            <Route path="/generate" element={<DownloadPortfolio />} /> {/* 포트폴리오 제작 페이지 라우트 */}
+            <Route path="/generate/:docId" element={<DownloadPortfolio />} /> {/* 특정 문서 제작 페이지 라우트 */}
+            <Route path="/preview" element={<Preview />} /> {/* 포트폴리오 미리보기 페이지 라우트 */}
+            <Route path="/preview/:docId" element={<Preview />} /> {/* 특정 문서 미리보기 페이지 라우트 */}
           </Routes>
         </BrowserRouter>
       </RecoilRoot>
