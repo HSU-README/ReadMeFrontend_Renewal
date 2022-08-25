68f66fa6f6682abe2358c5480760a68f1caa8b37
유준호 <yujunho@yujunhoui-MacBookPro.local>
2022-08-25 22:33:05 +0900

    검색 페이지 임시 완성

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,25 +1,71 @@
-/* eslint-disable array-callback-return */
 import React, { useEffect, useState } from 'react';
-import Header from 'components/header';
 import Footer from 'components/footer';
 import MainSelectCard from 'components/mainSelectCard/index';
 import Slider from 'react-slick';
+import Header from '../Home/header/Header';
 import { DocumentType } from '../../types/document';
 import { getSearchPortfolio } from '../../apis/portfolioApi';
+import prevBtn from '../../assets/icons/prevArrow.png';
+import nextBtn from '../../assets/icons/nextArrow.png';
+import notFoundIcon from '../../assets/icons/not_found_icon.png';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import './index.css';
 
 function SearchPage() {
   const [searchPortfolio, setSearchPortfolio] = useState<DocumentType[]>([]);
+
+  interface NextArrowProps {
+    className?: any;
+    style?: any;
+    onClick?: React.MouseEventHandler<HTMLImageElement>;
+  }
+  function NextArrow({ className, style, onClick }: NextArrowProps) {
+    // eslint-disable-next-line max-len
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
+    return (
+      // eslint-disable-next-line max-len
+      // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
+      <img
+        src={nextBtn}
+        alt="다음"
+        className={className}
+        style={{ ...style, width: '15px', height: '15px' }}
+        onClick={onClick}
+      />
+    );
+  }
+
+  function PrevArrow({ className, style, onClick }: NextArrowProps) {
+    // eslint-disable-next-line max-len
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
+    return (
+      // eslint-disable-next-line max-len
+      // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
+      <img
+        src={prevBtn}
+        alt="다음"
+        className={className}
+        style={{ ...style, width: '15px', height: '15px' }}
+        onClick={onClick}
+      />
+    );
+  }
+
   const settings = {
-    rows: 2,
-    dots: false,
-    arrows: true,
+    className: 'center',
+    centerMode: true,
     infinite: true,
-    speed: 300,
-    sliderPerRow: 3,
-    slidesToShow: searchPortfolio.length > 3 ? 3 : 1,
-    slidesToScroll: 1,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    speed: 500,
+    rows: 2,
+    dots: true,
+    slidesPerRow: 2,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
   };
+
   useEffect(() => {
     async function getSearhResult() {
       const searchParams = new URLSearchParams(window.location.search);
@@ -31,9 +77,9 @@ function SearchPage() {
   }, []);
 
   return (
-    <div>
+    <div className="cardView">
       <Header />
-      <div className="cardView">
+      <div>
         {searchPortfolio.length !== 0 ? (
           <Slider {...settings} className="slick">
             {searchPortfolio.map(
@@ -46,7 +92,10 @@ function SearchPage() {
             )}
           </Slider>
         ) : (
-          <div>아무것도 없어요</div>
+          <div className="notFound">
+            <img src={notFoundIcon} className="notFoundIcon" alt="아무것도 없어요" />
+            <div className="notFoundText">찾으시는 문서가 없어요!</div>
+          </div>
         )}
       </div>
       <Footer />
