83067da49e4786a86df8a5b875b36c1a880effe6
2jaebbang <72256238+2jaebbang@users.noreply.github.com>
2022-09-06 23:22:23 +0900

    fix: 유저 로그인 api 분리

--- src/apis/signApi.ts
+++ src/apis/signApi.ts
@@ -7,6 +7,24 @@ const serverApi = axios.create({
   },
 });
 
+export const loginUser = async (navigate: any, email: String, password: String) => {
+  await serverApi.post('https://hsureadme.herokuapp.com/api/v1/member/login', { email, password }).then((response) => {
+    if (response.data.code === 'S200') {
+      const userInfo = JSON.stringify(response.data.result);
+      // const successMessage = JSON.stringify(response.data.message);
+
+      localStorage.setItem('readme_login', 'true');
+      localStorage.setItem('readme_userInfo', userInfo);
+
+      navigate('/', {
+        state: {
+          isLoginSuccess: true,
+        },
+      });
+    }
+  });
+};
+
 const enrollUser = async (
   navigate: any,
   name: String,
