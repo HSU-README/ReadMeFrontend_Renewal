0b4cc009e0c864461841aaea0b0eab822daadf63
2jaebbang <72256238+2jaebbang@users.noreply.github.com>
2022-09-07 16:22:38 +0900

    remove: style 중복파일 삭제

--- src/colors.tsx
+++
@@ -1,9 +0,0 @@
-export default {
-  primary: '#F57842',
-  background: '#f8f9fa',
-  loginButton: 'black',
-  loginText: 'black',
-  loginBorder: '#F57842',
-  footerLine: '#F57842',
-  gray: '#707070',
-};
