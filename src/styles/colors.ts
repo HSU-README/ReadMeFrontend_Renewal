d9f8654389cab36081b137803ed854d2604efb09
2jaebbang <72256238+2jaebbang@users.noreply.github.com>
2022-09-07 16:23:57 +0900

    design: primary 색상 변경

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,9 +1,9 @@
 export default {
-  primary: '#F57842',
+  primary: '#f24444',
   background: '#f8f9fa',
   loginButton: 'black',
   loginText: 'black',
-  loginBorder: '#F57842',
-  footerLine: '#F57842',
+  loginBorder: '#f24444',
+  footerLine: '#f24444',
   gray: '#707070',
 };
