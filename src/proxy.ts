ebd1ebba64f211f960dfd333944bcf64543a19cf
LINKER <temp2temp@daum.net>
2022-09-20 07:17:50 +0900

    fix

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,9 +2,8 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 
 module.exports = (app:any): void => {
     app.use(
-        '/api/*',
-        createProxyMiddleware({
-            target: 'https://hsureadme.herokuapp.com',
+        createProxyMiddleware('/api', {
+            target: 'https://hsureadme.herokuapp.com/',
             changeOrigin: true,
         })
     );
