f9431a59f7c9bfbf774a700a66181ae133944531
LINKER <temp2temp@daum.net>
2022-09-20 05:41:28 +0900

    fix: cors error

--- src/apis/proxy.ts
+++ src/apis/proxy.ts
@@ -2,8 +2,9 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 
 module.exports = (app:any): void => {
     app.use(
+        '/v1/',
         createProxyMiddleware({
-            target: 'https://hsureadme.herokuapp.com/',
+            target: 'https://hsureadme.herokuapp.com/api',
             changeOrigin: true,
         })
     );
