1c29e00a3b16173581c17345d629e97cd05a6490
LINKER <temp2temp@daum.net>
2022-09-20 05:19:00 +0900

    fix: error

--- src/apis/proxy.ts
+++ src/apis/proxy.ts
@@ -2,13 +2,9 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 
 module.exports = (app:any): void => {
     app.use(
-        '/api',
         createProxyMiddleware({
-            target: 'https://hsureadme.herokuapp.com',
+            target: 'https://hsureadme.herokuapp.com/',
             changeOrigin: true,
-            pathRewrite: {
-                '^/api': '',
-            },
         })
     );
 };
