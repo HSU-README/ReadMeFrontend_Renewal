ee6eebb2bbe5487b47e7ea1bf30db8b00262288b
LINKER <temp2temp@daum.net>
2022-09-20 02:53:46 +0900

    fix: eslintrc

--- src/pages/Company/CompanyForm.tsx
+++ src/pages/Company/CompanyForm.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useEffect, useState } from 'react';
 import { Button } from '@mui/material';
 import Container, { ImageBox } from './style';
