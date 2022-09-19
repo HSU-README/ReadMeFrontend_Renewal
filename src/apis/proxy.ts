9867102ea81e3ba40adabf724922fd3284f3ced9
LINKER <temp2temp@daum.net>
2022-09-20 03:13:35 +0900

    feat: proxy 설정 / add: netlify.toml 생성

---
+++ src/apis/proxy.ts
@@ -0,0 +1,13 @@
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
+module.exports = (app:any): void => {
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: 'https://hsureadme.herokuapp.com',
+            pathRewrite: {
+                '^/api': '',
+            },
+        })
+    );
+};
