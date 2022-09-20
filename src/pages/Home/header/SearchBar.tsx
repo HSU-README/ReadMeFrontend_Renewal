5755d1c9fd17b24fbeadce27ebbe739f2f3db3a4
LINKER <temp2temp@daum.net>
2022-09-21 01:19:50 +0900

    fix: url fix

--- src/pages/Home/header/SearchBar.tsx
+++ src/pages/Home/header/SearchBar.tsx
@@ -19,7 +19,7 @@ function Searchbar() {
         size="small"
         onKeyUp={(e) => {
           if (e.key === 'Enter') {
-            document.location.href = `https://hsureadme.herokuapp.com/api/search?where=${searchText}`;
+            document.location.href = `api/search?where=${searchText}`;
           }
         }}
         InputProps={{
