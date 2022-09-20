8186478ca435f1d6d3fcc0fa5290950dda08b1dd
LINKER <temp2temp@daum.net>
2022-09-21 01:48:12 +0900

    fix

--- src/pages/Home/header/SearchBar.tsx
+++ src/pages/Home/header/SearchBar.tsx
@@ -19,7 +19,7 @@ function Searchbar() {
         size="small"
         onKeyUp={(e) => {
           if (e.key === 'Enter') {
-            document.location.href = `api/v1/doc/search?where=${searchText}`;
+            document.location.href = `https://hsureadme.herokuapp.com/api/v1/doc/search?where=${searchText}`;
           }
         }}
         InputProps={{
