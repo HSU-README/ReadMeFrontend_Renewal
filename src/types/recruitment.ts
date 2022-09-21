0df83cd0afd2b26f221e6c191c226341016ba78e
2jaebbang <72256238+2jaebbang@users.noreply.github.com>
2022-09-21 22:06:20 +0900

    feat: 채용공고 타입 추가

---
+++ src/types/recruitment.ts
@@ -0,0 +1,17 @@
+// 채용공고 타입
+export type RecruitmentType = {
+  id: number;
+  companyName: string;
+  content: string;
+  skill: string;
+  jobOpening: string;
+  region: string;
+  devision: string;
+  applyLink: string;
+  salary: string;
+};
+
+// 태그 타입
+export type TagType = {
+  name: string;
+};
