102cd0ba767d4d4a14bc4516af288d3633b0a906
LINKER <temp2temp@daum.net>
2022-09-24 12:17:07 +0900

    fix: eslint

--- src/setupProxy.ts
+++ src/setupProxy.ts
@@ -1,11 +1,12 @@
+// eslint-disable-next-line import/no-import-module-exports
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
-module.exports = (app:any): void => {
-    app.use(
-        createProxyMiddleware('/api', {
-            target: 'https://hsureadme.herokuapp.com/',
-            pathRewrite: { '^/api': '' },
-            changeOrigin: true,
-        })
-    );
+module.exports = (app: any): void => {
+  app.use(
+    createProxyMiddleware('/api', {
+      target: 'https://hsureadme.herokuapp.com/',
+      pathRewrite: { '^/api': '' },
+      changeOrigin: true,
+    }),
+  );
 };
