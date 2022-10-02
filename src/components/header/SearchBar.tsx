0660d00f911795f7ed05f16b59782130ecb2c013
LINKER <temp2temp@daum.net>
2022-10-02 12:50:49 +0900

    fix: searchbar cors

--- src/components/header/SearchBar.tsx
+++ src/components/header/SearchBar.tsx
@@ -41,7 +41,7 @@ function Searchbar() {
         placeholder={placeHolder}
         onKeyUp={(e) => {
           if (e.key === 'Enter') {
-            document.location.href = `https://readme-pro.netlify.app/search?where=${searchText}`;
+            document.location.href = `http://localhost:3000/search?where=${searchText}`;
           }
         }}
         value={searchText}
